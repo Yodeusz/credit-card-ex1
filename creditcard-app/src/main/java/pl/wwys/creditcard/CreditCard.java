7f78694a06991d03b11e949cae78d3be7a8043a9
Wojtek Wysoglad <yodeusz@gmail.com>
2018-10-11 09:06:28 +0000

    app improved

---
+++ creditcard-app/src/main/java/pl/wwys/creditcard/CreditCard.java
@@ -0,0 +1,12 @@
+package pl.wwys.creditcard;
+
+class CreditCard {
+    
+    public void assignLimit(double limit) {
+        
+    }
+    
+    public double getlimit() {
+        return 2000;
+    }
+}
