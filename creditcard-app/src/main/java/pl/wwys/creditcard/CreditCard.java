fbb77e5f0b0ccdc3c7da75e3bb06626375ad1f5b
Wojtek Wysoglad <yodeusz@gmail.com>
2018-10-18 08:30:22 +0000

    explain what is going on in get limit

--- creditcard-app/src/main/java/pl/wwys/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/wwys/creditcard/CreditCard.java
@@ -5,7 +5,7 @@ class CreditCard {
     public void assignLimit(double limit) {
         
     }
-    
+    //it will return proper number
     public double getlimit() {
         return 2000;
     }
